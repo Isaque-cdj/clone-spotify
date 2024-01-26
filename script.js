017cfde69416dbbb2b0a5176d8b9e949cca022e1
Isaque Coelho <isaquecoelho.dj@gmail.com>
2024-01-26 15:46:32 -0300

    Adequações referentes ao quarto dia

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 const searchInput = document.getElementById('search-input');
-const resultsAtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    ferch(url)
+    fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
-    const artistName = docment.getElementById('artist-name');
-    const artistImage = docment.getElementById('artist-img');
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
-    })
+    });
 
-    resultsAtist.classList.remove('hidden');
+    resultArtist.classList.remove('hidden');
 }
 
-
 document.addEventListener('input', function () {
-    const searchTerm = searchInput.ariaValueMax.toLowerCase();
+    const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultsAtist.classList.remove('hidden');
-        return;
+        resultArtist.classList.remove('hidden');
+        return
     }
 
     requestApi(searchTerm);
